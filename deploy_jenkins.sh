cb665c73726a2a508fa9e20dd2e955d95d142ada
zhujialun <jl.zhu3@htd.cn>
2021-01-21 18:31:51 +0800

    no message

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -14,21 +14,21 @@ CONTAINER="jenkins_demo"
 IMAGE=$CONTAINER":"$(date -d "today" +"%Y%m%d_%H%M%S")
 
 # 删除滚动更新残留的容器
-docker rm `docker ps -a | grep -w $CONTAINER"_"$CONTAINER | awk '{print $1}'`
+sudo docker rm `docker ps -a | grep -w $CONTAINER"_"$CONTAINER | awk '{print $1}'`
 # 强制删除滚动更新残留的镜像
-docker rmi --force `docker images | grep -w $CONTAINER | awk '{print $3}'`
+sudo docker rmi --force `docker images | grep -w $CONTAINER | awk '{print $3}'`
 
 # 创建新镜像
-docker build -t $IMAGE . && \
+sudo docker build -t $IMAGE . && \
 
 # 删除 docker-compose.jenkins.yml 文件，防止使用相同镜像
-rm -rf docker-compose.jenkins.yml && \
+sudo rm -rf docker-compose.jenkins.yml && \
 
 # 复制 docker-compose.src.yml 文件，防止污染原文件
-cp docker-compose.src.yml docker-compose.jenkins.yml && \
+sudo cp docker-compose.src.yml docker-compose.jenkins.yml && \
 
 # 替换镜像名标志位为最新镜像
-sed -i s%IMAGE_LATEST%$IMAGE%g docker-compose.jenkins.yml && \
+sudo sed -i s%IMAGE_LATEST%$IMAGE%g docker-compose.jenkins.yml && \
 
 # 使用 docker stack 启动服务
-docker stack deploy -c docker-compose.jenkins.yml $CONTAINER
+sudo docker stack deploy -c docker-compose.jenkins.yml $CONTAINER
